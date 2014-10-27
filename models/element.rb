840ae574a694df069fa1644ccd316a7423d5f50c
h4w5 <philip@h4w5.com>
2014-10-27 12:40:07 -0400

    begin BDD on Element class

--- models/element.rb
+++ models/element.rb
@@ -1,30 +1,30 @@
 class Element
+  attr_reader :atomic_number, :name, :symbol,
+               :electron_configuration, :category, :charges
+
+  def initialize(atomic_number, name, symbol, electron_configuration, category, charges)
+    @atomic_number = atomic_number
+    @name          = name
+    @symbol        = symbol
+    @electron_configuration = electron_configuration
+    @category      = category
+    @charges       = charges
+  end
 
   def self.find(atomic_number)
-    self.new
+    element = @@elements[atomic_number]
+    self.new(atomic_number, element[:name], element[:symbol],
+             element[:electron_configuration], element[:category], element[:charges])
   end
 
-  def atomic_number
-    1
-  end
-
-  def name
-    "Hydrogen"
-  end
-
-  def symbol
-    "H"
-  end
-
-  def electron_configuration
-    "1s1"
-  end
-
-  def category
-    "Hydrogen"
-  end
-
-  def charges
-    [-1]
-  end
+  @@elements = {
+    1 => {
+      name: "Hydrogen",
+      symbol: "H",
+      electron_configuration: "1s1",
+      category: "Hydrogen",
+      charges: [-1]
+    },
+    2 => {}
+  }
 end
