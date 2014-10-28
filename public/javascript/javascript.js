073bdf03f24a1a01d3afbfecfc86fa74723dd135
freejacque <freejacque@gmail.com>
2014-10-28 09:35:42 -0400

    template work

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -16,26 +16,38 @@ function seedAtomicNumbers(){
   };
 
   function Card(element){
-    this.atomicNumber = element.atomicNumber;
-    this.name = element.name;
-    this.symbol = element.symbol;
-    this.electronConfiguration = element.electronConfiguration;
-    this.category = element.category;
-    this.charge = element.charges[0];
-    this.charge2 = element.charges[1];
+    this.element = element;
+    // this.atomicNumber = element.atomicNumber;
+    // this.name = element.name;
+    // this.symbol = element.symbol;
+    // this.electronConfiguration = element.electronConfiguration;
+    // this.category = element.category;
+    // this.charge = element.charges[0];
+    // this.charge2 = element.charges[1];
   }
 
   Card.prototype = {
     template: _.template($("#card-template").html()),
 
     render: function(){
+      console.log(' view:render', this);
+      var temp = this.template({element: this.element});
+      this.$element = $(temp);
 
+      return this;
     },
 
     init: function(){
-
+      console.log(' view:init', this);
+      var view = this;
+      if (!this.$element){
+        view.render();
+        $("#card").append(view.$element);
+        console.log(this.$element);
+      }
     },
-  }
+  };
+
   function populateBoard(){
     for(var i=0; i < 18; i++){
     }
